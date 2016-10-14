cde95f4dea56c0fb47205d579a277f07e503bb93
cgottlieb <carl@lendup.com>
2016-10-14 13:28:23 -0700

    switched to 2 letter state codes

--- utils.js
+++ utils.js
@@ -5,145 +5,145 @@ const addresses = {
         city: '',
         zip: ''
     },
-    "Alabama": {
+    "AL": {
         street1: '600 Dexter Ave',
         street2: '',
         city: 'Montgomery',
         zip: '36130'
     },
-    "California": {
+    "CA": {
         street1: '123 Mission Street',
         street2: '',
         city: 'San Francisco',
         zip: '94105'
     },
-    "Florida": {
+    "FL": {
         street1: '400 S Monroe St',
         street2: '',
         city: 'Tallahassee',
         zip: '32399'
     },
-    "Hawaii": {
+    "HI": {
         street1: '415 S Beretania St.',
         street2: '',
         city: 'Honolulu',
         zip: '96813'
     },
-    "Idaho": {
+    "ID": {
         street1: '700 W Jefferson St.',
         street2: '',
         city: 'Boise',
         zip: '83720'
     },
-    "Illinois": {
+    "IL": {
         street1: '301 S 2nd Street',
         street2: '',
         city: 'Springfield',
         zip: '62706'
     },
-    "Indiana": {
+    "IN": {
         street1: '200 W Washington St',
         street2: '',
         city: 'Indianapolis',
         zip: '46204'
     },
-    "Kansas": {
+    "KS": {
         street1: '300 SW 10th St',
         street2: '',
         city: 'Topeka',
         zip: '66612'
     },
-    "Louisiana": {
+    "LA": {
         street1: '900 N 3rd St',
         street2: '',
         city: 'Baton Rouge',
         zip: '70802'
     },
-    "Maine": {
+    "ME": {
         street1: '230 State St',
         street2: '',
         city: 'Augusta',
         zip: '04330'
     },
-    "Michigan": {
+    "MI": {
         street1: '1903 W Michigan Ave',
         street2: '',
         city: 'Kalamazoo',
         zip: '49008'
     },
-    "Minnesota": {
+    "MN": {
         street1: '475 Rev Dr Martin Luther King Jr Boulevard',
         street2: '',
         city: 'St Paul',
         zip: '55155'
     },
-    "Mississippi": {
+    "MS": {
         street1: '400 High St',
         street2: '',
         city: 'Jackson',
         zip: '39201'
     },
-    "Missouri": {
+    "MO": {
         street1: '201 W Capitol Ave',
         street2: '',
         city: 'Jefferson City',
         zip: '65101'
     },
-    "New Mexico": {
+    "NM": {
         street1: '201 W. Marcy St',
         street2: '',
         city: 'Santa Fe',
         zip: '87501'
     },
-    "Ohio": { 
+    "OH": { 
         street1: '281 W. Lane Ave',
         street2: '', 
         city: 'Columbus', 
         zip: '43210' 
     },
-    "Oklahoma": {
+    "OK": {
         street1: '2300 N Lincoln Blvd',
         street2: '',
         city: 'Oklahoma City',
         zip: '73105'
     },
-    "Oregon": {
+    "OR": {
         street1: '900 Court St NE',
         street2: '',
         city: 'Salem',
         zip: '97301'
     },
-    "Tennessee": {
+    "TN": {
         street1: '600 Charlotte Ave',
         street2: '',
         city: 'Nashville',
         zip: '37243'
     },
-    "Texas": {
+    "TX": {
         street1: '1100 Congress Ave',
         street2: '',
         city: 'Austin',
         zip: '78701'
     },
-    "Utah": {
+    "UT": {
         street1: '350 State St',
         street2: '',
         city: 'Salt Lake City',
         zip: '84111'
     },
-    "Washington": {
+    "WA": {
         street1: '215 Sid Snyder Ave SW',
         street2: '',
         city: 'Olympia',
         zip: '98501'
     },
-    "Wisconsin": {
+    "WI": {
         street1: '400 W Canal St',
         street2: '',
         city: 'Milwaukee',
         zip: '53203'
     },
-    "Wyoming": {
+    "WY": {
         street1: '200 W 24th St',
         street2: '',
         city: 'Cheyenne',
