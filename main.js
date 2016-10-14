6ebcfd5eebbbbb07bd54de2f188359ad6288336e
danielearthy <daniel.earthy@lendup.com>
2016-10-14 16:23:55 -0400

    updates

--- main.js
+++ main.js
@@ -29,10 +29,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     });
 
-    // chrome.tabs.executeScript({
-    //   file:'utils.js'
-    // });
-
     chrome.tabs.executeScript({
       file:'populate.js'
     });
